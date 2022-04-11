54f9d314f37162c062db80b578f72e0792805494
AtanasKisyov <atanaskisyov9@gmail.com>
2022-04-11 20:38:42 +0300

    database update

--- book_reviews/settings.py
+++ book_reviews/settings.py
@@ -57,9 +57,9 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.getenv('DATABASE', 'postgres'),
         'USER': os.getenv('DB_USER', 'postgres'),
-        'PASSWORD': os.getenv('PASSWORD', '1123QwER'),
-        'HOST': os.getenv('HOST', 'localhost'),
-        'PORT': os.getenv('PORT', '5432'),
+        'PASSWORD': os.getenv('DB_PASSWORD', '1123QwER'),
+        'HOST': os.getenv('DB_HOST', 'localhost'),
+        'PORT': os.getenv('DB_PORT', '5432'),
     }
 }
 WSGI_APPLICATION = 'book_reviews.wsgi.application'
