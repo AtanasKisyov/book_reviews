cb29dd114cca647104b722144622cbd70998680a
AtanasKisyov <62992871+AtanasKisyov@users.noreply.github.com>
2022-04-12 14:57:26 +0300

    Update settings.py

--- book_reviews/settings.py
+++ book_reviews/settings.py
@@ -51,25 +51,18 @@ TEMPLATES = [
         },
     },
 ]
-if DEBUG:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'DATABASE_URL': os.environ.get('DATABASE_URL', ''),
+        'NAME': os.environ.get('DATABASE', 'postgres'),
+        'USER': os.environ.get('DB_USER', 'postgres'),
+        'PASSWORD': os.environ.get('DB_PASSWORD', '1123QwER'),
+        'HOST': os.environ.get('DB_HOST', '127.0.0.1'),
+        'PORT': os.environ.get('DB_PORT', '5431'),
     }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql',
-            'DATABASE_URL': os.environ.get('DATABASE_URL', ''),
-            'NAME': os.environ.get('DATABASE', 'postgres'),
-            'USER': os.environ.get('DB_USER', 'postgres'),
-            'PASSWORD': os.environ.get('DB_PASSWORD', '1123QwER'),
-            'HOST': os.environ.get('DB_HOST', '127.0.0.1'),
-            'PORT': os.environ.get('DB_PORT', '5431'),
-        }
-    }
+}
 
 WSGI_APPLICATION = 'book_reviews.wsgi.application'
 
